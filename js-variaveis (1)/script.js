36de950bbbd3eb4aa1a6b60384cea7b07e8bd428
gustavolima567 <gustavo.silveira.lima@escola.pr.gov.br>
2024-10-17 11:20:56 -0300

    js-variaveis

---
+++ js-variaveis (1)/script.js
@@ -0,0 +1,22 @@
+const nome = document.getElementById('nome');
+const sobrenome = document.getElementById('sobrenome')
+const email = document.getElementById('email')
+const telefone = document.getElementById('telefone')
+const idade = document.getElementById('idade')
+const sexo = document.getElementById('sexo')
+
+
+const novonome = "mary";
+const novosobrenome = "silva";
+const novoemail = "marysilva@escola.pr.gov.br";
+const novotelefone = "99 48702-2555";
+const novaidade = "19";
+const novosexo = "feminino";
+
+
+nome.innerText = novonome
+sobrenome.innerText = novosobrenome
+email.innerText = novoemail 
+telefone.innerText = novotelefone
+idade.innerText = novaidade
+sexo.innerText = novosexo
