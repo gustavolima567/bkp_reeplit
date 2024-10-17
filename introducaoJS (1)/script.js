c2ec74cef0b8e7bdef31eb73846abdd5a3d461e9
gustavolima567 <gustavo.silveira.lima@escola.pr.gov.br>
2024-10-17 11:37:30 -0300

    intruducaoJS

---
+++ introducaoJS (1)/script.js
@@ -0,0 +1,15 @@
+const idade = 17
+//crie uma variavel para cada um dos
+//dados listados ( nome, sobrenome, telefone, estacasado)
+//imprima cada um dos valores com comsole.log(<variavel>)
+
+
+console.log(idade)
+const nome = "joao "
+const sobrenome = "lima"
+const telefone = " 99999999"
+const estacasado = false
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estacasado)
